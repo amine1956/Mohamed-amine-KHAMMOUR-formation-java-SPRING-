47b684c3b6b11bcdc4a2c73b16cb2d66adbd39a5
amine1956 <amineklm983@gmail.com>
2022-03-09 20:56:38 +0100

    Activite2

--- Activeté2/demo/src/main/java/com/example/demo/entities/Patient.java
+++ Activeté2/demo/src/main/java/com/example/demo/entities/Patient.java
@@ -1,14 +1,11 @@
 package com.example.demo.entities;
 
-import jdk.nashorn.internal.objects.annotations.Getter;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import javax.persistence.*;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.Date;
 @ToString
 @Entity
@@ -18,7 +15,7 @@ public class Patient {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(length = 50)
-    private String nom;
+    private String Nom;
     @Temporal(TemporalType.DATE)
     private Date date;
     private boolean malade;
